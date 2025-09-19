43d9afc1aeda274658e59bcea0ffaa1940072306
Harshit Khandelwal <86138193+Harshit2756@users.noreply.github.com>
2025-09-19 09:53:14 +0000

    basic python functions

---
+++ 05_functions/01_duplication.py
@@ -0,0 +1,13 @@
+def print_order(name, chai_type):
+    print(f"{name} orderded {chai_type} chai")
+
+
+
+print_order("Aman", "Masala")
+print_order("Hitesh", "Ginger")
+print_order("Harshit", "Tulsi")
+
+
+
+
+
---
+++ 05_functions/04_readbility.py
@@ -0,0 +1,8 @@
+
+def calculate_bill(cups,price_per_cup):
+    return cups * price_per_cup
+
+my_bill = calculate_bill(45, 20)
+
+print(my_bill)
+
